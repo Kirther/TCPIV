b631d8fe6ea00ce60e78f47b2a04cc959d0c6a3f
Kirther <gabriel.fonini@gmail.com>
2020-11-03 02:34:33 -0300

    Escolha de língua funcionando e salvando na página de Settings

--- app/src/main/java/com/example/songbird/SettingsScreen.java
+++ app/src/main/java/com/example/songbird/SettingsScreen.java
@@ -1,12 +1,11 @@
 package com.example.songbird;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -16,8 +15,6 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import org.w3c.dom.Text;
 
 public class SettingsScreen extends AppCompatActivity {
 
@@ -29,6 +26,13 @@ public class SettingsScreen extends AppCompatActivity {
 
     Spinner language_Select;
     String [] languages = new String[] {"Português", "English"};
+
+    String selected_Language;
+
+    public static final String CREDITS_PREFS = "sharedPrefs";
+    public static final String LANGUAGE_CHOICE = "language_choice";
+    int spinner_Position = 0;
+    int index = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,14 +68,19 @@ public class SettingsScreen extends AppCompatActivity {
                 switch(position)
                 {
                     case 0:
-                        Context context = getApplicationContext();
+                        selected_Language = "Portuguese";
+                        UpdateView();
+                        saveData();
+
+                        /* Context context = getApplicationContext();
                         int duration = Toast.LENGTH_SHORT;
-                        Toast.makeText(context, "Portuguese Selected", duration).show();
+                        Toast.makeText(context, "Portuguese Selected", duration).show(); */
+
                         break;
                     case 1:
-                        Context context2 = getApplicationContext();
-                        int duration2 = Toast.LENGTH_SHORT;
-                        Toast.makeText(context2, "English Selected", duration2).show();
+                        selected_Language = "English";
+                        UpdateView();
+                        saveData();
                         break;
                 }
             }
@@ -81,10 +90,97 @@ public class SettingsScreen extends AppCompatActivity {
             }
         });
 
+        LoadData();
+        UpdateView();
+
+    }
+
+    private void UpdateView() {
+        settings_Text.setText(settingsString(selected_Language));
+        music_Text.setText(musicString(selected_Language));
+        language_Text.setText(languageString(selected_Language));
+    }
+
+    private void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(CREDITS_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(LANGUAGE_CHOICE, selected_Language);
+        editor.putInt("spinner_Position", language_Select.getSelectedItemPosition());
+
+        /* Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        Toast.makeText(context, String.valueOf(language_Select.getSelectedItemPosition()), duration).show(); */
+
+        editor.apply();
+    }
+
+    private void LoadData(){
+        SharedPreferences sharedPreferences = getSharedPreferences(CREDITS_PREFS, MODE_PRIVATE);
+
+        selected_Language = sharedPreferences.getString(LANGUAGE_CHOICE, "Portuguese");
+        index = sharedPreferences.getInt("spinner_Position", 0);
+        language_Select.setSelection(index);
     }
 
     private void ReturnToMainMenu() {
         Intent gameScreen = new Intent (this , GameMainMenu.class);
         startActivity(gameScreen);
+    }
+
+    public String settingsString(String args)
+    {
+        String text = args;
+        String displayText;
+        switch (text) {
+            case "Portuguese":
+                displayText = "Configurações";
+                break;
+
+            case "English":
+                displayText = "Settings";
+                break;
+
+            default:
+                displayText = "Configurações";
+        }
+        return displayText;
+    }
+
+    public String musicString(String args)
+    {
+        String text = args;
+        String displayText;
+        switch (text) {
+            case "Portuguese":
+                displayText = "Música";
+                break;
+
+            case "English":
+                displayText = "Music";
+                break;
+
+            default:
+                displayText = "Música";
+        }
+        return displayText;
+    }
+    public String languageString(String args)
+    {
+        String text = args;
+        String displayText;
+        switch (text) {
+            case "Portuguese":
+                displayText = "Língua";
+                break;
+
+            case "English":
+                displayText = "Language";
+                break;
+
+            default:
+                displayText = "Língua";
+        }
+        return displayText;
     }
 }
